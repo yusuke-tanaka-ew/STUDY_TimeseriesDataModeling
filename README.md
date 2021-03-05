d5577462f21de9def519afa9862de264a1862633
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-03-05 23:50:14 +0900

    ADD AR sample   Change phi & plot them

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # STUDY_TimeseriesDataModeling
+
+1. Textbook : 沖本竜義『経済・ファイナンスデータの軽量時系列分析』  
---
+++ Section1/ARPLot.R
@@ -0,0 +1,12 @@
+#AR(1) model
+# Move phi check y
+
+phi_list <- c(0.5,-0.5,1,-1,2,-2)
+
+get_ar_sample <- function(c,phi,sigma,n,initial_y){
+    
+}
+
+for(i in 1:length(phi_list)){
+  
+}
